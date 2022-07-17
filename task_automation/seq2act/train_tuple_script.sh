a4bae372b2eea7ce522ddd72b10775ffac080499
aburns4 <aburns4@bu.edu>
2022-07-17 11:32:39 -0400

    modifying train and eval scripts

--- task_automation/seq2act/train_tuple_script.sh
+++ task_automation/seq2act/train_tuple_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-python -m bin.seq2act_train_eval --exp_mode "train" \
+python -m seq2act.bin.seq2act_train_eval --exp_mode "train" \
                                          --experiment_dir ${PWD}"/ckpt_hparams/tuple_ua_ut_all_final2/" \
                                          --hparam_file ${PWD}"/ckpt_hparams/tuple_extract/" \
                                          --train_steps 1000000 \
