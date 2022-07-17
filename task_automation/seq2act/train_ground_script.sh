758f709b862b76638faec98d914499a7d3dbddbd
aburns4 <aburns4@bu.edu>
2022-07-17 11:16:37 -0400

    updating training scripts

---
+++ task_automation/seq2act/train_ground_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+
+python -m bin.seq2act_train_eval --exp_mode "train" \
+                                 --experiment_dir ${PWD}"/ckpt_hparams/ground_ricosca_ua_ut/" \
+                                 --hparam_file ${PWD}"/ckpt_hparams/grounding/" \
+                                 --train_steps 250000 \
+                                 --train_file_list ${PWD}"/data/rico_sca/ua_ut_tfexample/*.tfrecord" \
+                                 --train_batch_sizes "64" \
+                                 --train_source_list "rico_sca" \
+                                 --reference_checkpoint  ${PWD}"/ckpt_hparams/tuple_from_scratch_ua_ut"
---
+++ task_automation/seq2act/train_tuple_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+
+python -m bin.seq2act_train_eval --exp_mode "train" \
+                                         --experiment_dir ${PWD}"/ckpt_hparams/tuple_ua_ut_all_final2/" \
+                                         --hparam_file ${PWD}"/ckpt_hparams/tuple_extract/" \
+                                         --train_steps 1000000 \
+                                         --train_file_list ${PWD}"/data/android_howto/tfexample/*.tfrecord,${PWD}/data/rico_sca/ua_ut_tfexample_final/*.tfrecord" \
+                                         --train_batch_sizes "64,64" \
+                                         --train_source_list "android_howto,rico_sca"
