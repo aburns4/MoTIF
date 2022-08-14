e32c2f74d913f8f33535dd69ea884355b8240dab
aburns4 <aburns4@bu.edu>
2022-08-14 19:47:13 -0400

    update folder path again

--- task_automation/seq2act/motif_data_generation/dedup.sh
+++ task_automation/seq2act/motif_data_generation/dedup.sh
@@ -8,5 +8,5 @@ dir=${PWD}
 parentdir="$(dirname "$dir")"
 
 python all_in_one_motif_preprocess.py --data_dir ${parentdir}"/data/motif/raw/traces_02_14_21" \
-                                      --save_dir "processed_motif_deduped_3" \
+                                      --save_dir "processed_motif_deduped" \
                                       --dedup_cycles=true
